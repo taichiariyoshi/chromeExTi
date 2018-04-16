aa085fca077dda8b0a23139955bd5feef66f978d
Ariyoshi <taichi.ariyoshi@accenture.com>
2018-04-16 18:35:56 +0900

    add new files

---
+++ src/content_script.js
@@ -0,0 +1,18 @@
+﻿//----------------------
+$(function () {
+    let flag = false;
+    setInterval(function () {
+        document.title = (flag ? "■" : "◆") + document.title.replace(/^[■◆]/, '');
+        flag = !flag;
+    }, 1000);
+    //↑疎通確認コード
+})
+
+$(function(AutoClick){
+    document.getElementById("start!").onclick = function(AutoClick);
+
+
+    <g transform="translate(-8.13, -9)" fill="url(#svg-fill-linear__like)"><path d="M50.8 34.2c0-5.56-3.7-9.6-8.84-9.6-2.75 0-4.23 1-6.6 3.3-2.38-2.3-3.86-3.32-6.6-3.32-5.14 0-8.86 4.05-8.86 9.63 0 2.35.83 4.56 2.37 6.3l12.4 11.37c.38.34 1 .34 1.37 0l11.2-10.17.68-.62.25-.28.26-.27c1.54-1.75 2.38-3.97 2.38-6.32"></path></g>
+
+
+})
